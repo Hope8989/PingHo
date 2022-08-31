1883f6b2bbdd68dbe2558b43a1347885d7b7a9c3
Hope8989 <91896846+Hope8989@users.noreply.github.com>
2022-08-30 23:59:15 -0400

    try

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>我是何苹</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      check out
+
+      <a href="https://mynameishp2000.wixsite.com/pingho">my art</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
