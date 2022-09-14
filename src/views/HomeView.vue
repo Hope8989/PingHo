a7c941fba2ce019161cddbcff07fa079c00960bf
Hope8989 <91896846+Hope8989@users.noreply.github.com>
2022-09-13 22:24:10 -0400

    Update HomeView.vue
    play with the transition

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img mt="Vue logo" src="../assets/mnt.jpeg">
+    <img class= "mt" alt="mt" src="../assets/mnt.jpeg">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -18,7 +18,19 @@ export default {
 </script>
 
 <style type="text/css" scoped>
+.mt {
+  width: 80%;
+  min-width: 200px;
+  max-width: 400px;
+  border-radius: 25px;
+  transition: all 1s cubic-bezier(.35,-1.23,.83,1.32);
+}
 
+.mt:hover {
+  width: 90%;
+  min-width: 300px;
+  max-width: 600px;
+}
 
 
 </style>
