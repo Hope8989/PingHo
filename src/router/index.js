ca4d1f49147796a719db08ad24f2fac72cd7864f
Bestian Tang 小巴 <bestian@gmail.com>
2022-10-19 09:50:42 +0800

    s

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,7 @@ const routes = [
   },
    {
     path: '/bye',
-    name: 'about',
+    name: 'bye',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
